98506b43e9f2742a768dd619256b352f802bfa1a
Stalker1234567 <117269356+Stalker1234567@users.noreply.github.com>
2023-01-13 02:13:11 +0100

    Harvex v1.0
    The starting point of service development.

---
+++ src/main/java/com/robo/harvex/HarvexApplication.java
@@ -0,0 +1,13 @@
+package com.robo.harvex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarvexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarvexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/robo/harvex/HarvexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robo.harvex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarvexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
